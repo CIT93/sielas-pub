400b82dc7c6113bf1a86916d87aa708df13741de
Siela Sok <sielashawnsok@gmail.com>
2024-01-18 17:49:43 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,13 @@
+const myExample1 = 8;
+const myExample2 = 2;
+const myExample3 = 10;
+const myExample4 = 0;
+const myExample5 = 6;
+const myExample6 = 5;
+const myExample7 = 16;
+const myExample8 = 4;
+
+const total = myExample1 + myExample2 + myExample3 + myExample4 + myExample5 + myExample6 + myExample7 + myExample8;
+
+const totalFootprint = document.querySelector("h2");
+totalFootprint.textContent = "h2";
