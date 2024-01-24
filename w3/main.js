0ec5177e3515defded4b864418a6463c01d4b30a
Siela Sok <sielashawnsok@gmail.com>
2024-01-24 10:33:29 -0800

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,30 +1,30 @@
+function determineHouseHoldPts(numberInHousehold) {
+    console.log("inside the function");
+    if (numberInHousehold === 1) {
+        carbonFootprintPoints = 14;
+    } else if(numberInHousehold === 2) {
+        carbonFootprintPoints = 12;
+    } else if(numberInHousehold === 3) {
+        carbonFootprintPoints = 10;
+    } else if(numberInHousehold === 4) {
+        carbonFootprintPoints = 8;
+    } else if(numberInHousehold === 5) {
+        carbonFootprintPoints = 6;
+    } else if(numberInHousehold === 6) {
+        carbonFootprintPoints = 4;
+    } else if(numberInHousehold > 6) {
+        carbonFootprintPoints = 2;
+    } else {
+        console.log("no update to points")
+    }
+    console.log(`Based on the number of member of the household of ${numberInHousehold} the points would be ${carbonFootprintPoints}.`);
+}
 
 let carbonFootprintPoints = 0;
-let numberInHousehold = 1;
+// const numberInHousehold = 3;
 
-if(numberInHousehold === 1) {
-    carbonFootprintPoints = 14;
-}
-else if(numberInHousehold === 2) {
-    carbonFootprintPoints = 12;
-} 
-else if (numberInHousehold === 3) {
-    carbonFootprintPoints = 10;
-}
-else if(numberInHousehold === 4) {
-    carbonFootprintPoints = 8;
-}
-else if(numberInHousehold === 5) {
-    carbonFootprintPoints = 6;
-}
-else if(numberInHousehold === 6) {
-    carbonFootprintPoints = 4;
-}
-else if(numberInHousehold > 6) {
-    carbonFootprintPoints = 2;
-}
-else {
-    console.log("no update to points")
-}
 
-console.log(`Based on the number of member of the household of ${numberInHousehold} the points would be ${carbonFootprintPoints}.`);
+// global scope
+
+determineHouseHoldPts(3)
+determineHouseHoldPts(4)
