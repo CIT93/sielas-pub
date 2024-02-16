c930149dec6e07a611a6479c0995901bb437b849
Siela Sok <sielashawnsok@gmail.com>
2024-02-15 23:23:34 -0800

    my attempt at the code, before I review other students code

--- w6/main.js
+++ w6/main.js
@@ -54,11 +54,13 @@ function determineHouseSize(houseSize) {
 // }
 
 
-function start(numberInHousehold, houseSize) {
+function start(firstName, lastName, numberInHousehold, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(numberInHousehold);
   const houseSizePTS = determineHouseSize(houseSize);
   const total = houseHoldPTS + houseSizePTS;
   cfpData.push({
+    firstname: firstName,
+    lastname: lastName,
     members: numberInHousehold,
     size: houseSize,
     memberPoints: houseHoldPTS,
@@ -72,28 +74,25 @@ function displayOutput() {
     console.log(obj);
     const newH1 = document.createElement("h1");
     newH1.textContent = `Your Carbon Footprint is ${obj.cfpTotal}`;
+    const newH2 = document.createElement("h2");
+    newH2.textContent = `Name: ${obj.firstname} ${obj.lastname}`;
     const newP = document.createElement("p");
     newP.textContent = `The amount of people in the household is ${obj.members} so add ${obj.memberPoints} to your score,`;
     newP.textContent += ` the housesize is ${obj.size}, so add ${obj.sizePoints} to your score. Your total carbon foorprint score is ${obj.cfpTotal}.`;
     OUTPUT.appendChild(newH1);
+    OUTPUT.appendChild(newH2);
     OUTPUT.appendChild(newP);
   }
 }
 
 FORM.addEventListener("submit", function(e){
     e.preventDefault(); 
-    // console.log("I am inside the callback function");
-    // console.log(e);
     const firstName = FORM.firstname.value;
     const lastName = FORM.lastname.value;
     const numberInHousehold = parseInt(FORM.numberinhousehold.value);
     const houseSize = FORM.housesize.value;
-    start(numberInHousehold, houseSize);
+    start(firstName, lastName, numberInHousehold, houseSize);
     OUTPUT.innerHTML = "";
     displayOutput();
     FORM.reset();
 })
-
-// My apartment score is working, no problems.
-
-// To make sure that the data given is what we want
