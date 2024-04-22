227ed5e903a7e864511e021c767f3fc55f603625
Siela Sok <sielashawnsok@gmail.com>
2024-04-22 11:28:04 -0700

    try/catch with api call

--- w14/main.js
+++ w14/main.js
@@ -1,9 +1,21 @@
-// async function start() {
-//     const data = await fetch(`https://api.weather.gov/gridpoints/OKX/35,35/forecast`)
-//     const result = await data.json()
-//     console.log(result.properties.periods[1].shortForecast)
-// }
+async function start() {
+    try {
+    const data = await fetch(`https://api.weather.gov/gridpoints/OKX/35,35/forecast`)
+    const result = await data.json()
+    onSuccess(result.properties.periods[1].shortForecast)
+    }
+    catch(e) {
+        onError(error)
+    }
 
+}
+
+function onSuccess(success) {
+    console.log(`SUCCESS: ${success}`)
+}
+function onError(error) {
+    console.log(`ERROR: ${error}`)
+}
 // async function start2() {
 //     fetch(`https://api.weather.gov/gridpoints/OKX/35,35/forecast`)
 //     .then(data => data.json())
@@ -14,36 +26,36 @@
 
 // start();
 
-function getData() {
-    return new Promise(function(resolve,reject){ 
-        setTimeout(() => {
-            reject(`HERE IS YOUR DATA`)
-        }, 1)
-    })
-}
-
-// function onSuccess() {
-
+// function getData() {
+//     return new Promise(function(resolve,reject){ 
+//         setTimeout(() => {
+//             reject(`HERE IS YOUR DATA`)
+//         }, 1)
+//     })
 // }
 
-// function onFailure() {
+// // function onSuccess() {
+
+// // }
+
+// // function onFailure() {
     
+// // }
+// async function start() {
+//     try{
+//         const result = await getData()
+//         //SUCCESS
+//     } catch (error) {
+//         //FAILURE
+//     }   
 // }
-async function start() {
-    try{
-        const result = await getData()
-        //SUCCESS
-    } catch (error) {
-        //FAILURE
-    }   
-}
 
-// async function start2() {
-//    const result = await getData()
-//    .catch(error => {
-//     console.log(`Error: ${error}`)
-//    })
-//    console.log(result)
-// }
+// // async function start2() {
+// //    const result = await getData()
+// //    .catch(error => {
+// //     console.log(`Error: ${error}`)
+// //    })
+// //    console.log(result)
+// // }
 
 start()
